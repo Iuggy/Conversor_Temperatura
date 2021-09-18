d1189944b3581ce842bff81e726c19d7e1d98aa8
unknown <urylima5@gmail.com>
2021-09-18 10:47:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var nome =prompt("Olá digite seu Nome")
+var valorFarenheit=parseInt(prompt("Digite a temperatura em Farenheit"))
+var valorCelsius = (valorFarenheit - 32 ) * 5/9
+alert(nome + " Sua temperatura em Celsius é: " + valorCelsius)
+alert("Esse código é um desafio proposto na primeira aula da imersão Dev 😉")
